5659dd7463c8b7312b13dac6b750292db6c8ee67
Ali Ahamed Thowfeek <alitheoneman@gmail.com>
2024-04-21 19:22:40 +0530

    fix models

--- models.js
+++ models.js
@@ -1,49 +1,70 @@
 const { DataTypes } = require("sequelize");
 const db = require("./db");
 
-const Bank = db.define("Bank", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const Bank = db.define(
+  "bank",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
   },
-  name: {
-    type: DataTypes.STRING(100),
-    allowNull: false,
-  },
-});
+  {
+    tableName: "bank",
+    timestamps: false,
+  }
+);
 
-const Currency = db.define("Currency", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const Currency = db.define(
+  "currency",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    symbol: {
+      type: DataTypes.STRING(3),
+      allowNull: false,
+    },
   },
-  name: {
-    type: DataTypes.STRING(100),
-    allowNull: false,
-  },
-  symbol: {
-    type: DataTypes.STRING(3),
-    allowNull: false,
-  },
-});
+  {
+    tableName: "currency",
+    timestamps: false,
+  }
+);
 
-const BankRate = db.define("BankRate", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const BankRate = db.define(
+  "bank_rate",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    rate: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
   },
-  rate: {
-    type: DataTypes.FLOAT,
-    allowNull: false,
-  },
-  date: {
-    type: DataTypes.DATE,
-    allowNull: false,
-  },
-});
+  {
+    tableName: "bank_rate",
+    timestamps: false,
+  }
+);
 
 BankRate.belongsTo(Bank, { foreignKey: "bank_id" });
 BankRate.belongsTo(Currency, { foreignKey: "currency_id" });
